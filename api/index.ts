3753a998d1fadabbaddcc25a4fea31e4822b0189
Kachkynov Alikhan <t8t228@gmail.com>
2025-04-30 19:36:41 +0600

    [api] initialization

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import config from "./config";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+const run = async () => {
+  await mongoose.connect(config.db);
+
+  app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+  });
+
+  process.on("exit", () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch(console.error);
+
